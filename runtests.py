4ec30614de65622e3cae44df4728ee14f3049fd6
Øyvind Saltvik <oyvind.saltvik@gmail.com>
2012-07-12 18:31:24 +0200

    add a comma

--- runtests.py
+++ runtests.py
@@ -6,5 +6,5 @@ if __name__ == '__main__':
     runtests_parse(configure=configure,
         test_labels_prefix='shop_categories',
         default_test_labels=['shop_categories'],
-        tmp_dir_prefix='django-shop-categories'
+        tmp_dir_prefix='django-shop-categories',
         ROOT_URLCONF='shop_categories.test_utils.project.urls')
