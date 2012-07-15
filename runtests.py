358caa5f9ac0e46147a64501f011fca6845f1e4f
Øyvind Saltvik <oyvind.saltvik@gmail.com>
2012-07-15 23:00:59 +0200

    new test setup

--- runtests.py
+++ runtests.py
@@ -1,10 +1,40 @@
 #!/usr/bin/env python
-from djeasytests.runtests import runtests_parse
-from shop_categories.test_utils.cli import configure
-    
-if __name__ == '__main__':    
-    runtests_parse(configure=configure,
-        test_labels_prefix='shop_categories',
-        default_test_labels=['shop_categories'],
-        tmp_dir_prefix='django-shop-categories',
-        ROOT_URLCONF='shop_categories.test_utils.project.urls')
+from djeasytests.testsetup import TestSetup, default_settings
+
+default_settings.update(dict(
+    ROOT_URLCONF='shop_categories.test_utils.project.urls',
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': 'shopexampledb.sqlite',
+        }
+    },
+    INSTALLED_APPS = [
+        'django.contrib.auth',
+        'django.contrib.contenttypes',
+        'django.contrib.sessions',
+        'django.contrib.admin',
+        'django.contrib.sites',
+        'django.contrib.staticfiles',
+        'treeadmin',
+        'shop_categories',
+        'shop_categories.test_utils.project'
+    ],
+    LANGUAGE_CODE = "en",
+    LANGUAGES = (
+        ('en', gettext('English')),
+        ('fr', gettext('French')),
+        ('de', gettext('German')),
+        ('pt-BR', gettext("Brazil")),
+        ('nl', gettext("Dutch")),
+    ),
+    SOUTH_TESTS_MIGRATE = False,
+    ROOT_URLCONF = ROOT_URLCONF,
+    SHOP_PRODUCT_MODEL = 'shop_categories.test_utils.project.models.product.Product',
+    SHOP_CATEGORIES_CATEGORY_MODEL = 'shop_categories.test_utils.project.models.category.Category'
+))
+
+testsetup = TestSetup(appname='shop_example', default_settings=default_settings)
+
+if __name__ == '__main__':
+    testsetup.run('shell')
