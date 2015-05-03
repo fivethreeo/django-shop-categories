325d5900e7c09fc7def5105dbc01f1a05685a05c
Øyvind Saltvik <oyvind.saltvik@gmail.com>
2015-05-03 17:51:05 +0200

    no defaults

--- shop_categories/urls.py
+++ shop_categories/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls.defaults import patterns, url
+from django.conf.urls import patterns, url
 from shop_categories.views import CategoryProductDetailView, CategoryShopListView
 from shop.models.productmodel import Product
 
